84c89cf6995a340f5c624c6a2586fb6d4536b57f
finn <finnandshadow@gmail.com>
2021-07-03 11:51:33 -0400

    Frender crash testing

--- main.cpp
+++ main.cpp
@@ -1,3 +1,4 @@
+#include <ios>
 #include <iostream>
 #include "Frender/Frender.hh"
 #include "GLTFLoader.hh"
@@ -5,21 +6,54 @@
 #include <stb/stb_image.h>
 
 #include <glm/gtc/matrix_transform.hpp>
+#include <string>
+#include <sstream>
 
 Frender::RenderObjectRef ro;
+Frender::Window* window;
+Frender::Renderer* renderer;
+glm::mat4 camera;
+float angle = 0;
+
+double elapsed_time;
 
 void loop(float delta)
 {
+    // Show FPS
+    elapsed_time += delta;
+
+    if (elapsed_time > 0.5)
+    {
+        std::ostringstream outs;
+        outs.precision(3); // Set precision of numbers
+        // outs.fill(3);
+
+        outs << std::fixed << "FPS: " << renderer->frame_rate; // << " EFT: " << window->time_time << " EFPS: " << 1/window->time_time;
+
+        window->setWindowTitle(outs.str());
+    }
+
     auto t = ro.getTransform();
     t = glm::rotate(t, 3.14f * delta, glm::vec3(0, 1, 0));
     ro.setTransform(t);
+    
+    angle += ((3.14/2) * delta);
+    camera = glm::mat4();
+    camera = glm::rotate(camera, angle, glm::vec3(0, 1, 0));
+    camera = glm::translate(camera, glm::vec3(0, 0, 2.5));
+
+    renderer->setCamera(camera);
 }
 
 int main(int, char**)
 {
     Frender::Window w({640, 480, "Hello Frender"});
+    // w.setVsync(false);
+    w.setVsync(true);
 
     Frender::Renderer r(640, 480);
+    renderer = &r;
+    window = &w;
 
     std::vector<Frender::Vertex> vertices = {
         {0.5f,  0.5f, 0.0f , 1, 0}, // top right
@@ -41,6 +75,7 @@ int main(int, char**)
     glm::mat4 cam_mat;
     cam_mat = glm::translate(cam_mat, glm::vec3(0, 0, 1));
     r.setCamera(cam_mat);
+    camera = cam_mat;
 
     r.getMaterial(mat)->uniforms.set("color", glm::vec3(0.6, 1, 0.6));
     r.getMaterial(mat)->uniforms.set("has_texture", 1);
